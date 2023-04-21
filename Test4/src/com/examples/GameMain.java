dc20663a9a4d2d95016ba0ec6df8db61e366361b
tqvivek <122796418+tqvivek@users.noreply.github.com>
2023-04-21 09:43:16 +0530

    Add files via upload

--- Test4/src/com/examples/GameMain.java
+++ Test4/src/com/examples/GameMain.java
@@ -4,18 +4,21 @@ public class GameMain {
 
 	public static void main(String[] args) {
 
-		Game g1 = new BasketBall();
-		Game g2 = new FootBall();
+		// Game g1 = new BasketBall();
+		// Game g2 = new FootBall();
 
+		Game g1;
+		g1 = new BasketBall();
 		g1.setUp();
 		g1.rules();
 		g1.start();
 
 		System.out.println("-------------------------");
 
-		g2.setUp();
-		g2.rules();
-		g2.start();
+		g1 = new FootBall();
+		g1.setUp();
+		g1.rules();
+		g1.start();
 
 	}
 
